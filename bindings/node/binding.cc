a066c06a10483ab6a30ab6563a1921bc8929d555
Aymeric Wibo <obiwac@gmail.com>
2024-09-01 00:28:14 +0200

    ci/test: Don't do `cargo test` for now

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_Flamingo();
+extern "C" TSLanguage * tree_sitter_flamingo();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_Flamingo());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_flamingo());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("Flamingo").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("flamingo").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_Flamingo_binding, Init)
+NODE_MODULE(tree_sitter_flamingo_binding, Init)
 
 }  // namespace
