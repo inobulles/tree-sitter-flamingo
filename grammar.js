1ef89a6b200935e5ecb2b7d2edad14582bb7f77c
Aymeric Wibo <obiwac@gmail.com>
2024-09-02 23:33:56 +0200

    return: Fix associativity

--- grammar.js
+++ grammar.js
@@ -75,7 +75,7 @@ module.exports = grammar({
 
 		print: $ => seq("print", field("msg", $.expression)),
 		assert: $ => seq("assert", field("test", $.expression)),
-		return: $ => seq("return", field("rv", $.expression)),
+		return: $ => prec.right(seq("return", optional(field("rv", $.expression)))),
 
 		expression: $ =>
 			prec(
--- src/scanner.c
+++ src/scanner.c
@@ -1,8 +1,8 @@
 // This Source Form is subject to the terms of the AQUA Software License,
 // v. 1.0. Copyright (c) 2024 Aymeric Wibo
 
-#include "tree_sitter/parser.h"
 #include "tree_sitter/alloc.h"
+#include "tree_sitter/parser.h"
 
 typedef struct {
 } Scanner;
@@ -12,26 +12,30 @@ enum TokenType {
 };
 */
 
-void* tree_sitter_flamingo_external_scanner_create(void) {
-	Scanner* const scanner = ts_calloc(1, sizeof *scanner);
-	return scanner;
+void *tree_sitter_flamingo_external_scanner_create(void) {
+  Scanner *const scanner = ts_calloc(1, sizeof *scanner);
+  return scanner;
 }
 
-void tree_sitter_flamingo_external_scanner_destroy(void* payload) {
-	Scanner* const scanner = payload;
-	ts_free(scanner);
+void tree_sitter_flamingo_external_scanner_destroy(void *payload) {
+  Scanner *const scanner = payload;
+  ts_free(scanner);
 }
 
-unsigned tree_sitter_flamingo_external_scanner_serialize(void* payload, char* buffer) {
-	// Nothing to serialize.
-	return 0;
+unsigned tree_sitter_flamingo_external_scanner_serialize(void *payload,
+                                                         char *buffer) {
+  // Nothing to serialize.
+  return 0;
 }
 
-void tree_sitter_flamingo_external_scanner_deserialize(void* payload, char const* buffer, unsigned length) {
-	// Nothing to deserialize.
+void tree_sitter_flamingo_external_scanner_deserialize(void *payload,
+                                                       char const *buffer,
+                                                       unsigned length) {
+  // Nothing to deserialize.
 }
 
-bool tree_sitter_flamingo_external_scanner_scan(void* payload, TSLexer* lexer, bool const* valid_symbols) {
-	// Nothing to scan.
-	return false;
+bool tree_sitter_flamingo_external_scanner_scan(void *payload, TSLexer *lexer,
+                                                bool const *valid_symbols) {
+  // Nothing to scan.
+  return false;
 }
